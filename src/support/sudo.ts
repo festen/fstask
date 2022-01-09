f2ff8563e5fe9651b88713acd1d402eb59e8d191
David Festen <d.festen@gmail.com>
2022-01-09 10:34:25 +0100

    chore: update sudo to include bash sudo

--- src/support/sudo.ts
+++ src/support/sudo.ts
@@ -6,7 +6,11 @@ export async function withContext<T> (
 ): Promise<T> {
   let child: ProcessPromise<ProcessOutput> | undefined
   if (sudo) {
+    console.log('Some scripts eed sudo access, your password will be requested upfront.')
+    console.log('You might be prompted to answer your sudo password twice.')
+    console.log('This is because homebrew requires a bash shell, which might not be your current shell.')
     await $`sudo -v`
+    await $`bash -c "sudo-v"`
     child = nothrow($`while true; do sudo -v; sleep 6000; done`)
   }
   try {
